9b772ae0542844178ba510cca0bc60e46ef54d6a
Yashu-maheshwari <cute.yashu09@gmail.com>
2026-07-10 09:53:08 +0530

    Add visual branding image helpers

--- ame-bazaar-astra-child/inc/helpers.php
+++ ame-bazaar-astra-child/inc/helpers.php
@@ -18,6 +18,12 @@ function ame_bazaar_asset_path( $relative_path ) {
 }
 
 function ame_bazaar_get_custom_logo_url() {
+	$primary_logo_url = ame_bazaar_get_visual_branding_image_url( 'primary_logo', 'full' );
+
+	if ( $primary_logo_url ) {
+		return $primary_logo_url;
+	}
+
 	$custom_logo_id = get_theme_mod( 'custom_logo' );
 
 	if ( $custom_logo_id ) {
@@ -35,4 +41,37 @@ function ame_bazaar_get_brand_name() {
 	$brand = get_bloginfo( 'name' );
 
 	return $brand ? $brand : 'AME Bazaar';
+}
+
+function ame_bazaar_get_visual_branding_image_id( $key ) {
+	if ( ! function_exists( 'ame_bazaar_get_visual_branding_images' ) ) {
+		return 0;
+	}
+
+	$images = ame_bazaar_get_visual_branding_images();
+	$key    = sanitize_key( $key );
+
+	return isset( $images[ $key ] ) ? absint( $images[ $key ] ) : 0;
+}
+
+function ame_bazaar_get_visual_branding_image_url( $key, $size = 'full' ) {
+	$image_id = ame_bazaar_get_visual_branding_image_id( $key );
+
+	if ( ! $image_id ) {
+		return '';
+	}
+
+	$image_url = wp_get_attachment_image_url( $image_id, $size );
+
+	return $image_url ? $image_url : '';
+}
+
+function ame_bazaar_get_visual_branding_image( $key, $size = 'full', $attr = array() ) {
+	$image_id = ame_bazaar_get_visual_branding_image_id( $key );
+
+	if ( ! $image_id ) {
+		return '';
+	}
+
+	return wp_get_attachment_image( $image_id, $size, false, $attr );
 }
